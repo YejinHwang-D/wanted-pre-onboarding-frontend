3f36c7233d807be9c67ebefed04296e3cbf7833f
YejinHwang-D <hyj3463@naver.com>
2023-04-10 00:05:11 +0900

    feat: Header 컴포넌트 구현 완료

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,16 +1,44 @@
-import styles from './Header.module.css';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const LinkStyled = styled(Link)`
+  text-decoration: none;
+  color: white;
+`;
+const HeaderStyled = styled.header`
+  display: flex;
+  justify-content: space-between;
+  background-color: #8d8daa;
+  color: white;
+`;
+const LogoDiv = styled.div`
+  margin-left: 3em;
+`;
+const SignDiv = styled.div`
+  margin-right: 3em;
+  display: flex;
+`;
+const P = styled.p`
+  margin-left: 1em;
+`;
 
 function Header() {
   return (
-    <header className={styles.header}>
-      <div className={styles.logo}>
-        <p>프리온보딩 프론트엔드</p>
-      </div>
-      <div className={styles.sign}>
-        <p>로그인</p>
-        <p>회원가입</p>
-      </div>
-    </header>
+    <HeaderStyled>
+      <LogoDiv>
+        <LinkStyled to="/">
+          <p>프리온보딩 프론트엔드</p>
+        </LinkStyled>
+      </LogoDiv>
+      <SignDiv>
+        <LinkStyled to="/signin">
+          <P>로그인</P>
+        </LinkStyled>
+        <LinkStyled to="signup">
+          <P>회원가입</P>
+        </LinkStyled>
+      </SignDiv>
+    </HeaderStyled>
   );
 }
 
