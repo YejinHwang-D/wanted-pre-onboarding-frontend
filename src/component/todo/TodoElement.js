99b057e390488766e6d7f03580f883fea23d8079
YejinHwang-D <hyj3463@naver.com>
2023-04-11 16:45:35 +0900

    feat: checked 수정 업데이트 완료

--- src/component/todo/TodoElement.js
+++ src/component/todo/TodoElement.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import axios from 'axios';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { useEffect } from 'react';
 
 const Li = styled.li`
@@ -9,19 +9,33 @@ const Li = styled.li`
 
 function TodoElement({ data, getData }) {
   const token = localStorage.getItem('JWT');
+  const checked = useRef();
 
-  const [isUpdate, setIsUpdate] = useState(false);
+  const [isUpdatemode, setIsUpdatemode] = useState(false);
+  function isUpdatemodeHandler() {
+    setInputs({ ...inputs, todo: data.todo });
+    setIsUpdatemode(!isUpdatemode);
+  }
+
   const [inputs, setInputs] = useState({
-    isCompleted: false,
+    isCompleted: data.isCompleted,
     todo: data.todo,
   });
+
+  useEffect(() => {
+    if (inputs.isCompleted) {
+      checked.current.checked = true;
+    } else {
+      checked.current.checked = false;
+    }
+  }, [inputs]);
 
   function changeHandler(e) {
     const { name, value } = e.target;
     setInputs({ ...inputs, [name]: value });
   }
 
-  async function updateHandler() {
+  async function updateHandler(enteredData) {
     const res = await axios({
       url: `https://www.pre-onboarding-selection-task.shop/todos/${data.id}`,
       method: 'put',
@@ -29,12 +43,10 @@ function TodoElement({ data, getData }) {
         Authorization: `Bearer ${token}`,
         'Content-Type': 'application/json',
       },
-      data: inputs,
+      data: enteredData,
     });
     if (res.status !== 200) {
       throw new Error('[ERROR] 정상적으로 업데이트되지 않았습니다.');
-    } else {
-      setIsUpdate(!isUpdate);
     }
   }
 
@@ -52,25 +64,25 @@ function TodoElement({ data, getData }) {
     getData();
   }
 
-  function isUpdateHandler() {
-    setInputs({ ...inputs, todo: data.todo });
-    setIsUpdate(!isUpdate);
+  async function isCompletedHandler() {
+    const enteredData = {
+      isCompleted: !inputs.isCompleted,
+      todo: inputs.todo,
+    };
+    updateHandler(enteredData);
+    setInputs({ ...inputs, isCompleted: !inputs.isCompleted });
   }
 
-  async function isCompletedHandler() {
-    setInputs({ ...inputs, isCompleted: !inputs.isCompleted });
-    updateHandler();
+  async function isUpdateHandler() {
+    updateHandler(inputs);
+    setIsUpdatemode(!isUpdatemode);
   }
 
   return (
     <Li>
       <label>
-        {data.isCompleted ? (
-          <input type="checkbox" checked onClick={isCompletedHandler} />
-        ) : (
-          <input type="checkbox" onClick={isCompletedHandler} />
-        )}
-        {isUpdate ? (
+        <input type="checkbox" onChange={isCompletedHandler} ref={checked} />
+        {isUpdatemode ? (
           <div>
             <input
               data-testid="modify-input"
@@ -78,17 +90,17 @@ function TodoElement({ data, getData }) {
               name="todo"
               onChange={changeHandler}
             />
-            <button data-testid="submit-button" onClick={updateHandler}>
+            <button data-testid="submit-button" onClick={isUpdateHandler}>
               제출
             </button>
-            <button data-testid="cancel-button" onClick={isUpdateHandler}>
+            <button data-testid="cancel-button" onClick={isUpdatemodeHandler}>
               취소
             </button>
           </div>
         ) : (
           <div>
             <span>{inputs.todo}</span>
-            <button onClick={isUpdateHandler}>수정</button>
+            <button onClick={isUpdatemodeHandler}>수정</button>
             <button onClick={deleteHandler}>삭제</button>
           </div>
         )}
